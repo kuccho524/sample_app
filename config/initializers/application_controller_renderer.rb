e99aabb381cd0713aca6462898b6a57a5f5dbca0
kuccho524 <kucchachachanocha@gmail.com>
2021-04-29 00:14:15 +0000

    commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c1fb1097eba4b8407a3485fb5d344f0f588423644a31314d8052b7a8886766ca152eaa403de3d6b94867d1cb7a7e628504fe7c27ccee2c576f63fb8f744994dc'
